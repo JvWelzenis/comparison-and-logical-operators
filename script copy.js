48b2e4d0badef0e2c1bf1d68fac1553cd513f6f7
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-09 21:39:14 +0200

    Add files via upload

---
+++ script copy.js
@@ -0,0 +1,47 @@
+/*const age = 18;
+if (age >=18) {
+    console.log ("je mag naar binnen");
+} else {
+    console.log ("je mag niet naar binnen");
+} */
+
+const isFemale = true;
+if (isFemale) {
+    console.log ('je mag naar binnen');
+} else{
+    console.log ('je mag niet naar binnen');
+}
+
+const driverStatus = 'bob';
+if (driverStatus === 'bob') {
+    console.log ('je mag rijden');
+} else { 
+    console.log ('je mag niet rijden');
+}
+
+const age = 20;
+if (age >=18 && age <=25 && age === 18 && age ===25) {
+    console.log('Je krijgt 50% korting!');
+} else {
+    console.log("jij niet!");
+}
+
+const name = 'sywert';
+if (name === 'Bram' || name === 'Sarah') {
+    console.log('je krijgt een gratis biertje!');
+} else {
+    console.log('jij niet!');
+}
+
+const totalAmount = 30;
+if (totalAmount >=25 || totalAmount <=50) {
+    console.log('gratis vega bitterballen!');
+} if (totalAmount >=50 || totalAmount <=100) {
+    console.log ('gratis nachos!');
+}  if (totalAmount >=100) {
+    console.log ('gratis champagne!');
+} else {
+    console.log ('jij krijgt niets');
+}
+
+//twijfel aan bovenstaande... 
---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 18;
+if (age >=18) {
+    console.log ("je mag naar binnen");
+} else {
+    console.log ("je mag niet naar binnen");
+} 
+
+const isFemale = true;
+if (isFemale) {
+    console.log ('je mag naar binnen');
+} else{
+    console.log ('je mag niet naar binnen');
+}
+
+const driverStatus = 'bob';
+if (driverStatus === 'bob') {
+    console.log ('je mag rijden');
+} else { 
+    console.log ('je mag niet rijden');
+}
+
